a80f2c90e0264b20f99954b06653b87e845fbedd
rossyspike <jmcamero.24@est.ucab.edu.ve>
2026-09-15 22:30:33 -0400

    test for ranges /api/cursor

--- backend/tests/endpoints/api/image/cursor/get/test.sh
+++ backend/tests/endpoints/api/image/cursor/get/test.sh
@@ -8,4 +8,36 @@ if [ "$RESULT" != "TEST PASSED" ]; then
 fi
 # TODO: bound check for limit and current url param
 echo "WARNING. NO TEST FOR: bound check for limit and current url param"
+
+log() {
+
+  echo "$1" >&2
+}
+
+send_request() {
+  log "ATTEMPTING TO UPLOAD IMAGE: \"$1\""
+  echo "curl -v -s       \"http://$HOST/api/image/cursor?current=\"$2\"&limit=\"$3\" \"" >&2
+  RESULT=$(curl -v -s "http://$HOST/api/image/cursor?current=$2&limit=$3 ")
+  log "RESULT:=$RESULT"
+  echo "$RESULT"
+
+}
+
+TEST=$(send_request -1 2)
+if [ "$TEST" = "Bad headers" ] || [ "$TEST" = "Invalid current parameter" ]; then
+  echo "Test passed"
+
+else
+  echo "Test failed"
+  exit 1
+fi
+TEST=$(send_request 5 -2)
+if [ "$TEST" = "Bad headers" ] || [ "$TEST" = "Invalid limit parameter" ]; then
+  echo "Test passed"
+
+else
+  echo "Test failed"
+  exit 1
+fi
+
 exit 0
